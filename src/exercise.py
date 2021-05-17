96e3d230785988e33c8e4bf4b9e06fc6cd595d89
MrSullivan <>
2021-05-17 20:24:14 +0000

    lots

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #write your code below this line
 
+    print_from_number_to_one(5)
+
+def print_from_number_to_one(number):
+  i = 5
+  while (i <= number):
+      print(i)
+      i -= 1
+      if (i == -1):
+        break
+
 if __name__ == '__main__':
     main()
